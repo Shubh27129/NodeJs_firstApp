18f3b428754652bbef752d1659668419c2d0be17
Shubh27129 <shubham27129@gmail.com>
2023-08-06 13:17:56 +0530

    Request / Response Based on the url the user hits

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const http= require('http')
 
 const server= http.createServer((req,res)=>{
+    const url= req.url;
+    if(url==='/node')
+    {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
-    res.write('<h1>Shubham Kumar Sinha</h1>')
+    res.write('<head><title>My Title Page</title></head>')
+    res.write('<body><h1>Welcome to my Node Js project</h1></body>')
     res.write('</html>')
+    res.end()
+    }
+    else if(url==='/home')
+    {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My Title Page</title></head>')
+    res.write('<body><h1>Welcome home</h1></body>')
+    res.write('</html>')
+    res.end()
+    }
+    else if(url==='/about')
+    {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My Title Page</title></head>')
+    res.write('<body><h1>Welcome to About Us page</h1></body>')
+    res.write('</html>')
+    res.end()
+    }
+
 })
 
-server.listen(4000)
+server.listen(4001)
